2d3df11f1b6bd88c2ebb40d616ea5206f7e2d4cb
Afkre <ahmetefkere@gmail.com>
2023-04-10 18:20:55 +0200

    2023.04.10-4

--- Desing/homepage.php
+++ Desing/homepage.php
@@ -1,1 +1,25 @@
 <?php include 'header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Klinik Otomation</title>
+</head>
+<body>
+    <div class="vor_nach_name">
+        <h4>Hallo, Vorname Nachname</h4>
+    </div>
+    <div class="mitte_div" id="termin_div">
+
+    </div>
+    <div class="mitte_div" id="hausarzt_div">
+        <h3>Haus Arzt</h3>
+        <p>Ihr Hausarzt hat keine freien Arbeitszeiten.</p>
+    </div>
+    
+</body>
+</html>
